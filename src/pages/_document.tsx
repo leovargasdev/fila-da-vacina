341a7a06e460f799d27fb4a19b0e554d0da446a7
Leonardo Vargas <leu1607@hotmail.com>
2021-02-19 09:25:49 -0300

    Adiciona componente pergunta

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -44,7 +44,7 @@ export default class MyDocument extends Document {
           <meta charSet="utf-8" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
           <link
-            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;700;800&display=swap"
+            href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700;800&display=swap"
             rel="stylesheet"
           />
         </Head>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,23 @@
 import React from 'react'
 
 import { Banner } from '../components/Banner'
-import { Container } from '../styles/home'
+import { Container, Content, Button } from '../styles/home'
 
 function Home() {
   return (
     <Container>
       <Banner />
-      <h1>a</h1>
+      <Content>
+        <p>Qual e a sua idade?</p>
+        <div>
+          <Button type="button">menos 18</Button>
+          <Button type="button" active>
+            18 a 59
+          </Button>
+          <Button type="button">60 a 74</Button>
+          <Button type="button">mais de 75</Button>
+        </div>
+      </Content>
     </Container>
   )
 }
